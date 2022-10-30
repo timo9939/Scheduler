2c678d39d0888a1f74a45a0751837fc30120f59a
timo9939 <timothylau20199@gmail.com>
2022-10-30 12:05:52 +0000

    layout of scheduler

---
+++ assi5.js
@@ -0,0 +1,7 @@
+let timeNow= moment();
+let day=moment();
+
+$('#timeDay').text(day.format("dddd"));
+$('#timeDisplay').text(timeNow.format("MMM Do, YYYY"));
+console.log(timeDisplay);
+timeDisplay;
